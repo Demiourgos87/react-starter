55881f8e0cf8898281fd169c87f215ef9cc90e7e
Bojan Knezevic <bojan.knezevic@gmail.com>
2025-03-23 23:07:03 +0100

    Test component tests, make counter

--- src/components/test-component/test-component.test.tsx
+++ src/components/test-component/test-component.test.tsx
@@ -1,7 +1,24 @@
-import { render, screen } from '@testing-library/react';
 import TestComponent from '@components/test-component/test-component';
+import { fireEvent, render, screen } from '@testing-library/react';
 
-test('renders TestComponent component', () => {
-  render(<TestComponent />);
-  expect(screen.getByRole('heading', { name: /Test Component/i })).toBeInTheDocument();
+describe('Test component', () => {
+  it('Render title', async () => {
+    render(<TestComponent />);
+
+    const title = await screen.findByTestId('component-title');
+
+    expect(title).toHaveTextContent('Test Component');
+  });
+
+  it('Increment counter', async () => {
+    render(<TestComponent />);
+
+    const counter = await screen.findByTestId('component-counter');
+    const button = await screen.findByTestId('component-increment-button');
+
+    expect(counter).toHaveTextContent('Count: 0');
+
+    fireEvent.click(button);
+    expect(counter).toHaveTextContent('Count: 1');
+  });
 });
--- src/components/test-component/test-component.tsx
+++ src/components/test-component/test-component.tsx
@@ -1,19 +1,21 @@
 import { useState } from 'react';
 
 const TestComponent = () => {
-  const [text, setText] = useState('This is a component for testing purposes.');
+  const [counter, setCounter] = useState(0);
 
   const handleOnClick = () => {
-    console.log('clicked');
-    setText('Some changed text');
+    const newCount = counter + 1;
+    setCounter(newCount);
   };
 
   return (
     <section className="test-component">
-      <h1>Test Component</h1>
-      <p>{text}</p>
+      <h1 data-testid="component-title">Test Component</h1>
+      <p data-testid="component-counter">Count: {counter}</p>
 
-      <button onClick={handleOnClick}>Change text</button>
+      <button onClick={handleOnClick} data-testid="component-increment-button">
+        Increment
+      </button>
     </section>
   );
 };
