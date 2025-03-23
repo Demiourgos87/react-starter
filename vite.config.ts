cf6b0421595b904aa591af6ef69c2fcce7f92dde
Bojan Knezevic <bojan.knezevic@gmail.com>
2025-03-24 00:14:22 +0100

    define absolute imports

--- vite.config.ts
+++ vite.config.ts
@@ -8,8 +8,15 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
+      '@api': path.resolve(__dirname, 'src/api'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
       '@components': path.resolve(__dirname, 'src/components'),
+      '@constants': path.resolve(__dirname, 'src/constants'),
       '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@lib': path.resolve(__dirname, 'src/lib'),
+      '@stores': path.resolve(__dirname, 'src/stores'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@custom-types': path.resolve(__dirname, 'src/types'),
       '@utils': path.resolve(__dirname, 'src/utils'),
     },
   },
